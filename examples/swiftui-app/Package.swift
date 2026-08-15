654635cd404cb96df7dd72d47927977b53ac43f6
bongjava <bongjava@gmail.com>
2026-08-15 10:56:44 +0900

    Bump examples/swiftui-app/Package.swift to 1.0.1

--- examples/swiftui-app/Package.swift
+++ examples/swiftui-app/Package.swift
@@ -4,7 +4,7 @@ import PackageDescription
 let package = Package(
     name: "ExampleSwiftUIApp",
     dependencies: [
-        .package(url: "https://github.com/bongjava/my-design-system.git", exact: "1.0.0")
+        .package(url: "https://github.com/bongjava/my-design-system.git", exact: "1.0.1")
     ],
     targets: [
         .target(name: "ExampleSwiftUIApp")

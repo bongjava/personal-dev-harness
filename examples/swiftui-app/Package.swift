b5a5488576b35915798e94568298db61d0d4edf8
bongjava <bongjava@gmail.com>
2026-08-15 10:53:02 +0900

    Update design system dependency URL in Package.swift

--- examples/swiftui-app/Package.swift
+++ examples/swiftui-app/Package.swift
@@ -4,7 +4,7 @@ import PackageDescription
 let package = Package(
     name: "ExampleSwiftUIApp",
     dependencies: [
-        .package(url: "https://github.com/my-org/my-design-system.git", exact: "1.0.0")
+        .package(url: "https://github.com/bongjava/my-design-system.git", exact: "1.0.0")
     ],
     targets: [
         .target(name: "ExampleSwiftUIApp")
